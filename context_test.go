242e6e643e7a7df7f5ba7bdd85a8ed350e871158
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-12-18 19:57:58 +0700

    Problem Goroutine Leak

--- context_test.go
+++ context_test.go
@@ -3,6 +3,7 @@ package golangcontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -47,4 +48,41 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c")) // tetap dapat data valuenya, karena dia akan mencari ke parentnya, dan parent dari contextF adalah contextC
 	fmt.Println(contextF.Value("b")) // tidak dapat data (nil), karena b berada di contextB, sedangkan contextB bukan parent dari contextF
 	fmt.Println(contextA.Value("b")) // tidak dapat data (nil), meskipun b berada di contextB dan contextB adalah child dari contextA. Dikarenakan tidak bisa mengambil data child nya
+}
+
+// buat function CreateCounter dengan balikannya adalah channel yang tipenya integer
+func CreateCounter() chan int {
+
+	destination := make(chan int)
+
+	// untuk isi dari channelnya kita menggunakan goroutine
+	// jika goroutine selesai dikerjakan, maka kita close juga channelnya
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			// data counter kita kirim ke channel destination
+			// karena ini perulang tanpa henti, ini menyebabkan goroutine leak
+			destination <- counter
+			counter++
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	// ambil destination
+	destination := CreateCounter()
+
+	// perulangan untuk mencetak destination
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	// disini masih ada Goroutine Leak (nyala terus)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
